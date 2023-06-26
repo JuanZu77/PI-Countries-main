35b36dad813ba83d32e3c724ed9a0e3c6b941515
JuanZu77 <juanvzubiri77@gmail.com>
2023-06-25 23:44:43 -0300

    update back completed

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
+
 const CountriesRoutes = require ('../routes/CountriesRoutes');
 
 const ActivitiesRoutes = require ('../routes/ActivitiesRoutes');
