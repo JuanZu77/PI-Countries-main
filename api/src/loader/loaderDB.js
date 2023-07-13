d33dc6226438586114feaaa5b3db1bb5a6099779
JuanZu77 <juanvzubiri77@gmail.com>
2023-07-12 23:36:47 -0300

    update urlapi

--- api/src/loader/loaderDB.js
+++ api/src/loader/loaderDB.js
@@ -1,6 +1,7 @@
 const axios = require ("axios");
 const { Country } = require('../db');
-const urlapi= "https://rest-countries.up.railway.app/v2/all";
+const urlapi= "https://restcountries.com/v3/all";
+//"https://rest-countries.up.railway.app/v2/all";
 
 const loaderDB=async()=>{
 	const getapi= await axios.get(urlapi);
@@ -10,11 +11,11 @@ const loaderDB=async()=>{
 			await Country.findOrCreate({ 
 				//creo los registros en la tabla donde:
 				where:{
-					id:e.alpha3Code,
-					name:e.name, 
-					flag: e.flags.svg, 
+					id:e.cca3, 
+					name:e.name.common, 
+					flag: (e.flags)[0], //es array
 					continent:e.region,
-					capital:e.capital ? e.capital : "There is no capital",
+					capital:e.capital ? e.capital[0] : "There is no capital",
 					subregion:e.subregion ? e.subregion : "There is no Subregion",
 					area:e.area+" Km2",
 					population:e.population
